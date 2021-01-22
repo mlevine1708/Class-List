85de20481225172310020307263b1251fd697c8f
Micah Levine <micah.levine@outlook.com>
2021-01-22 11:02:54 -0500

    added more seed data

--- Class List/db/seeds.rb
+++ Class List/db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Student.create(name: 'Eli', parent: 'Micah Levine', email: 'micah.levine@outlook.com', phone_number: 667-555-5555, user_id: 1, teacher_id: 1)
+Student.create(name: 'Fozzie', parent: 'Micah Levine', email: 'micah.levine@outlook.com', phone_number: 667-555-5555, user_id: 1, teacher_id: 2)
+Student.create(name: 'Ariana', parent: 'Brett Lebowitz', email: 'brett@brett.com', phone_number: 410-555-5555, user_id: 1, teacher_id: 3)
 
 Teacher.create(name: 'Levine', grade: 8)
 Teacher.create(name: 'Lebowitz', grade: 4)
