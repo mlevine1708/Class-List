169f541578f35563d3fbd77c2393920d514a672b
Micah Levine <micah.levine@outlook.com>
2021-01-22 11:09:28 -0500

    more data

--- Class List/db/seeds.rb
+++ Class List/db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Student.create(name: 'Eli', parent: 'Micah Levine', email: 'micah.levine@outlook.com', phone_number: 667-555-5555, user_id: 1, teacher_id: 1)
-Student.create(name: 'Fozzie', parent: 'Micah Levine', email: 'micah.levine@outlook.com', phone_number: 667-555-5555, user_id: 1, teacher_id: 2)
-Student.create(name: 'Ariana', parent: 'Brett Lebowitz', email: 'brett@brett.com', phone_number: 410-555-5555, user_id: 1, teacher_id: 3)
+Student.create(name: 'Eli', grade: 8, parent: 'Micah Levine', email: 'micah.levine@outlook.com', phone_number: 667-555-5555, user_id: 1, teacher_id: 1)
+Student.create(name: 'Fozzie', grade: 4, parent: 'Micah Levine', email: 'micah.levine@outlook.com', phone_number: 667-555-5555, user_id: 1, teacher_id: 2)
+Student.create(name: 'Ariana', grade: 2, parent: 'Brett Lebowitz', email: 'brett@brett.com', phone_number: 410-555-5555, user_id: 1, teacher_id: 3)
 
 Teacher.create(name: 'Levine', grade: 8)
 Teacher.create(name: 'Lebowitz', grade: 4)
