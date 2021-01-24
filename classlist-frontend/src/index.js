57735001c81022d9aef55b566c0bb3730f50eeff
Micah Levine <micah.levine@outlook.com>
2021-01-23 20:09:03 -0500

    adding fetch

--- classlist-frontend/src/index.js
+++ classlist-frontend/src/index.js
@@ -1,1 +1,5 @@
 console.log("testing...")
+const BACKEND_URL = 'localhost:3000';
+fetch(`${BACKEND_URL}/test`)
+  .then(response => response.json())
+  .then(parsedResponse => console.log(parsedResponse));
