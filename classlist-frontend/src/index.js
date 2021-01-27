198422440a9fff124aa25f96fc6faedb567450f2
Micah Levine <micah.levine@outlook.com>
2021-01-26 20:50:28 -0500

    added ajax

--- classlist-frontend/src/index.js
+++ classlist-frontend/src/index.js
@@ -1,5 +1,21 @@
 console.log("testing...")
-const BACKEND_URL = 'localhost:3000';
+const BACKEND_URL = 'http://localhost:3000';
 fetch(`${BACKEND_URL}/test`)
   .then(response => response.json())
-  .then(parsedResponse => console.log(parsedResponse));
+  .then(parsedResponse => renderList(parsedResponse));
+
+function renderList(response) {
+    var element = document.querySelector(".student-list")
+    element.innerHTML="<ul>"
+    for (var i=0; i<response["students"].length;i++){
+        console.log(response["students"][i])
+        element.innerHTML+="<li>"+response["students"][i].name+"</li>"
+    }
+    element.innerHTML+="<ul>"
+   
+}
+document.addEventListener("click",function(e){
+    fetch(`${BACKEND_URL}/students/new`)
+  .then(response => response.json())
+  .then(parsedResponse => renderList(parsedResponse))
+})
