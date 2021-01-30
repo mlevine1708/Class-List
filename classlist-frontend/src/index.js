5da146f96da887a792fe16ccbe7b3b67b5a7f1a6
Micah Levine <micah.levine@outlook.com>
2021-01-30 16:18:03 -0500

    fixed data table

--- classlist-frontend/src/index.js
+++ classlist-frontend/src/index.js
@@ -6,12 +6,28 @@ fetch(`${BACKEND_URL}/test`)
 
 function renderList(response) {
     var element = document.querySelector(".student-list")
-    element.innerHTML="<ul>"
+    //element.appendChild()="<ul>"
     for (var i=0; i<response["students"].length;i++){
+        var row = document.createElement("tr")
+        var studentName = document.createElement("td")
+        var grade = document.createElement("td")
+        var parentName = document.createElement("td")
+        var phoneNumber = document.createElement("td")
+        var emailAddress = document.createElement("td")
         console.log(response["students"][i])
-        element.innerHTML+="<li>"+response["students"][i].name+"</li>"
+        studentName.textContent = response["students"][i].name
+        grade.textContent = response["students"][i].grade
+        parentName.textContent = response["students"][i].parent
+        phoneNumber.textContent = response["students"][i].phone_number
+        emailAddress.textContent = response["students"][i].email
+        row.append(studentName)
+        row.append(grade)
+        row.append(parentName)
+        row.append(phoneNumber)
+        row.append(emailAddress)
+        element.append(row)
     }
-    element.innerHTML+="<ul>"
+    
    
 }
 document.addEventListener("click",function(e){
