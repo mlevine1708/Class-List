f8e0d13acf2b6b4589affe940e33844f361f83a7
Micah Levine <micah.levine@outlook.com>
2021-02-06 13:02:56 -0500

    added readme

--- Class List/db/seeds.rb
+++ Class List/db/seeds.rb
@@ -5,9 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Student.create(name: 'Eli', grade: 8, parent: 'Micah Levine', email: 'micah.levine@outlook.com', phone_number: 667-555-5555, user_id: 1, teacher_id: 1)
-Student.create(name: 'Fozzie', grade: 4, parent: 'Micah Levine', email: 'micah.levine@outlook.com', phone_number: 667-555-5555, user_id: 1, teacher_id: 2)
-Student.create(name: 'Ariana', grade: 2, parent: 'Brett Lebowitz', email: 'brett@brett.com', phone_number: 410-555-5555, user_id: 1, teacher_id: 3)
+Student.create(name: 'Eli', grade: 8, parent: 'Micah Levine', email: 'micah.levine@outlook.com', phone_number: 667-555-5555, teacher_id: 1)
+Student.create(name: 'Fozzie', grade: 4, parent: 'Micah Levine', email: 'micah.levine@outlook.com', phone_number: 667-555-5555, teacher_id: 2)
+Student.create(name: 'Ariana', grade: 2, parent: 'Brett Lebowitz', email: 'brett@brett.com', phone_number: 410-555-5555, teacher_id: 3)
+Student.create(name: 'Art', grade: 7, parent: 'Eli Levine', email: 'art@gmail.com', phone_number: 410-555-3214, teacher_id: 4)
 
 Teacher.create(name: 'Levine', grade: 8)
 Teacher.create(name: 'Lebowitz', grade: 4)
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Virtual-School-Help
+# Class List
 
+This website application is a “Student Contacts” website, so teachers can keep a running contact list of their students, both past and present. As a teacher myself, I thought this was an interesting idea since we always have our students’ contact information, but once they leave our classes, the database with that information is deleted. If we want to get in touch with previous parents, etc., it is difficult to do. So, I thought this might be a helpful tool for teachers.
 
-Hello World!
+This is built on Ruby/Rails backend and Javascript/HTML/CSS frontend. Users are able to add new students, along with contact information. They may also delete students and update student information.
